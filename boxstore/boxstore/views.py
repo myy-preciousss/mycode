555eaf056fecf1cdf8882e4c43e96a85d7de6c64
myy-preciousss <preciouspimentel@gmail.com>
2023-06-16 15:35:54 +0000

    http response codes

---
+++ boxstore/boxstore/views.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# imports from Django
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseNotFound
+
+# This view will return a 404 response
+def ierror(request):
+    return HttpResponseNotFound("Page was not found")
+    
+def success(request):
+    return HttpResponse("Page was found")
+
+def customheader(request):
+    x = {}
+    x['learning'] = 'Django' # string:string
+    x['speed'] = 55          # string:integer
+    
+    return HttpResponse(headers=x)  # adds our custom headers to the response
+
+def customcode(request):
+    return HttpResponse("Working on that", status=201)  # return the response code 201 "created"
+
+## Challenge 01
+def clash(request):
+    return HttpResponse(status=409) # return response code 409 "conflict"
+
+def absent(request):
+    return HttpResponse(status=410) # return response code 410 "gone"
