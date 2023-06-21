46c5f62af517ffa4b1cad67ec9e6cd93a706d69d
myy-preciousss <preciouspimentel@gmail.com>
2023-06-21 14:48:56 -0500

    Added Challenge 01

--- gotapi/iceAndFire04.py
+++ gotapi/iceAndFire04.py
@@ -4,9 +4,17 @@
 # https://anapioficeandfire.com/Documentation
 
 import requests
-import pprint
+from prettyprinter import pprint
 
 AOIF_CHAR = "https://www.anapioficeandfire.com/api/characters/"
+
+def name_finder(got_list):
+        names = []
+        for name in got_list:
+                got_name_resp = requests.get(name)
+                got_name_dj   = got_name_resp.json()
+                names.append(got_name_dj['name'])
+        return names
 
 def main():
         ## Ask user for input
@@ -17,7 +25,23 @@ def main():
 
         ## Decode the response
         got_dj = gotresp.json()
-        pprint.pprint(got_dj)
+        # pprint.pprint(got_dj)
+
+        ## Challenge 01: Return the house(s) affiliated with the character 
+        ## looked up, along with a list of books they appear in
+        character_name = got_dj["name"]
+        alias = got_dj["aliases"]
+        print(f"\nYou looked up {character_name}, also known as {alias[0]}.")
+        
+        print(f"\n{character_name} belongs to the following houses: ")
+        for allegiance in name_finder(got_dj.get("allegiances")):
+                print(allegiance)
+
+        print(f"\n{character_name} also appears in the following books:")
+
+        for book in name_finder(got_dj.get("books")):
+                print(book)
+        
 
 if __name__ == "__main__":
         main()
