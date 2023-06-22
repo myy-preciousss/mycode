5fcbd42a23202a442e843033503b119906366d6a
myy-preciousss <preciouspimentel@gmail.com>
2023-06-22 09:42:07 -0500

    Renamed pandas folder to panda_learn. Added code customization in pikachu03.py.

--- pokemon/pikachu03.py
+++ pokemon/pikachu03.py
@@ -11,15 +11,20 @@ import requests
 ## python3 -m pip install pandas
 import pandas
 
-ITEMURL = "http://pokeapi.co/api/v2/item/"
+from prettyprinter import pprint
+import os
+import json
+
+ITEMURL = "https://pokeapi.co/api/v2/item/"
+POKEURL = "https://pokeapi.co/api/v2/pokemon/"
 
 def main():
 
     # Make HTTP GET request using requests
     # and decode JSON attachment as pythonic data structure
-    # Also, append the URL ITEMURL with a parameter to return 1000
+    # Also, append the URL ITEMURL with a parameter to return all
     # items in one response
-    items = requests.get(f"{ITEMURL}?limit=1000")
+    items = requests.get(f"{ITEMURL}?limit=2050")
     items = items.json()
 
     # create a list to store items with the word searched on
@@ -43,7 +48,7 @@ def main():
     ## list all words containing matched word
     print(f"There are {len(finishedlist)} words that contain the word '{args.searchword}' in the Pokemon Item API!")
     print(f"List of Pokemon items containing '{args.searchword}': ")
-    print(matchedwords)
+    pprint(matchedwords)
 
     ## export to excel with pandas
     # make a dataframe from our data
@@ -53,8 +58,37 @@ def main():
     # python -m pip install openpyxl
     # index=False prevents the index from our dataframe from
     # being written into the data
-    itemsdf.to_excel("pokemonitems.xlsx", index=False)
+    cwd = os.getcwd()
+    itemsdf.to_excel(f"{cwd}/pokemonitems.xlsx", index=False)
 
+    ## Code Customization -b
+    ## Export a list of all pokemon as plaintext, JSON, & Excel
+    pokemon = requests.get(f"{POKEURL}?limit=1281")
+    # print(f"Pokemon HTTP: {type(pokemon)}")
+    pokemon = pokemon.json()
+    poke_names = []
+
+    for poke in pokemon["results"]:
+        poke_names.append(poke["name"])
+    
+    poke_df = pandas.DataFrame(poke_names)
+
+    # print(f"Pokemon JSON: {type(pokemon)}")
+    # print(f"Poke_names: {type(poke_names)}")
+    # print(f"Poke DF: {type(poke_df)}")
+    
+    ## To plaintext
+    poke_df.to_csv(f"{cwd}/pokemonlist.csv", index=False)
+    
+    ## To JSON
+    with open(f"{cwd}/pokemonlist.csv", 'w') as jsonfile:
+        jsonfile.write(json.dumps(poke_names))
+    
+    ## To Excel
+    poke_df.to_excel(f"{cwd}/pokemonlist.xlsx", index=False)
+
+    ## Code Customization -e
+    
     print("Gotta catch 'em all!")
 
 if __name__ == "__main__":
