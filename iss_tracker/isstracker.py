e8c98029b2c56ebee29e8eb41e2d0caeb34897b7
myy-preciousss <preciouspimentel@gmail.com>
2023-06-26 15:26:51 -0500

    ISS API Challenge

--- iss_tracker/isstracker.py
+++ iss_tracker/isstracker.py
@@ -1,13 +1,34 @@
+# API challenge ISS
+import json
+from prettyprinter import pprint
 import requests
+import reverse_geocoder as rg
+from time import strftime, localtime
 
 URL = "http://api.open-notify.org/iss-now.json"
 
 def main():
     resp = requests.get(URL).json()
 
+    # Get Timestamp
+    epoch = resp["timestamp"]
+    local = strftime('%Y-%m-%d %H:%M:%S', localtime(epoch))
+
+    # Get coordinates
+    lon = resp["iss_position"]["longitude"]
+    lat = resp["iss_position"]["latitude"]
+    coords_tuple = (lat, lon)
+    location = rg.search(coords_tuple, verbose=False) #Returns as a list/ordered dict
+    country = location[0]["cc"]
+    city = location[0]["name"]
+
+    # pprint(location)
+
     print("CURRENT LOCATION OF ISS:")
-    print("Lon:", resp["iss_position"]["longitude"])
-    print("Lat:", resp["iss_position"]["latitude"])
+    print(f'> Timestamp: {local}')
+    print(f"> Lon: {lon}")
+    print(f"> Lat: {lat}")
+    print(f"> City/Country: {city}, {country}")
 
 if __name__ == "__main__":
     main()
